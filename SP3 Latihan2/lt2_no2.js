0f39a6244bbe96bed1d3f904b3163873cbfa70c3
Ariful Farhan Lubis <48346730+Arifular@users.noreply.github.com>
2021-08-04 21:24:20 +0700

    Add files via upload

---
+++ SP3 Latihan2/lt2_no2.js
@@ -0,0 +1,13 @@
+// var kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90];
+// var hasil = kodeUnix.filter(a => a >= 60)
+// console.log(hasil);
+
+let panggilFilter = () => {
+    panggilFilter2()
+}
+
+let panggilFilter2 = () => {
+    var kodeUnix = [23, 89,67,29,192,6,2,129,21,872,891,901,70,61,78,62,32,90,90];
+    console.log(kodeUnix.filter(a => a >= 60));
+}
+panggilFilter(panggilFilter2)
